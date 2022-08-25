d278dc242d9efffe51900d82173acde56edfd08b
Pedro Lizarraga <plizarraga86@gmail.com>
2022-08-25 15:40:20 -0700

    removed old code

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -2,12 +2,11 @@ import homeViewTemplate from '../views/home.html?raw';
 
 export default (): HTMLElement => {
   const divElement = document.createElement('section');
-  // divElement.
   divElement.innerHTML = homeViewTemplate;
 
   const btnClick = divElement.querySelector('#btnClick');
   btnClick!.addEventListener('click', () => {
-    alert('clicked');
+    alert('clicked!');
   });
 
   return divElement;
