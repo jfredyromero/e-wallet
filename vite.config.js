2f4a78487ec39dba94829f5d8333410bc9a63afe
Jhon Fredy Romero <jfredyrom@gmail.com>
2022-08-26 08:43:05 -0500

    Linting for html files added

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [eslint()],
+	plugins: [
+		eslint({
+			lintOnStart: true,
+			include: ['**/*.js', '**/*.ts', '**/*.html'],
+			fix: true,
+		}),
+	],
 });
