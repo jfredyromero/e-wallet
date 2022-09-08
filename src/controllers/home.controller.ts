72328ca9fde1bad46881d0cdd97724676d7f6e90
Jhon Fredy Romero <jfredyrom@gmail.com>
2022-09-08 12:06:13 -0500

    Styles added for home view. New eslint rules added.

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -3,11 +3,5 @@ import homeViewTemplate from '../views/home.html?raw';
 export default (): HTMLElement => {
 	const sectionElement = document.createElement('section');
 	sectionElement.innerHTML = homeViewTemplate;
-
-	const btnClick = sectionElement.querySelector('#btn-click');
-	btnClick!.addEventListener('click', () => {
-		alert('clicked!');
-	});
-
 	return sectionElement;
 };
