0dfd9f2dfe33045a81abc4862f92155fa7311ee6
Pedro Lizarraga <plizarraga86@gmail.com>
2022-09-22 15:44:38 -0700

    added list items to transactions page

--- src/controllers/transactions.controller.ts
+++ src/controllers/transactions.controller.ts
@@ -1,7 +1,21 @@
-import transactionsViewTemplate from '../views/transactions.html?raw';
+import template from '../views/transactions.html?raw';
+// eslint-disable-next-line max-len
+import transactionItemComponent from '../components/transaction-item/transaction-item.component.js';
+import { getAllTransactions } from '../services/wallet.service.js';
 
 export default (): HTMLElement => {
+	const transactions = getAllTransactions();
+	let html = '';
+	transactions.forEach(transaction => {
+		html += transactionItemComponent(transaction);
+	});
+
 	const sectionElement = document.createElement('section');
-	sectionElement.innerHTML = transactionsViewTemplate;
+	sectionElement.innerHTML = template;
+
+	const transactionListElement =
+		sectionElement.getElementsByClassName('transactions-list');
+	transactionListElement[0].innerHTML = html;
+
 	return sectionElement;
 };
