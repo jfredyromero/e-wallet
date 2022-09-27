b563ed431cab0803b5124e7377882532b37cf967
Pedro Lizarraga <plizarraga86@gmail.com>
2022-09-26 21:44:13 -0700

    fixed UT for hero component

--- src/app/components/hero/hero.component.spec.ts
+++ src/app/components/hero/hero.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeroComponent } from './hero.component';
 
@@ -8,7 +9,7 @@ describe('HeroComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [HeroComponent],
+			imports: [HeroComponent, RouterTestingModule],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(HeroComponent);
