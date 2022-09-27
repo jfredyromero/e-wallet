3236d95c3809e163f59fe47a286dface42885e1c
Pedro Lizarraga <plizarraga86@gmail.com>
2022-09-26 18:03:29 -0700

    Created header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-	declarations: [AppComponent, NavbarComponent, FooterComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, HeaderComponent, FooterComponent],
 	providers: [],
 	bootstrap: [AppComponent],
 })
