c6f136b7d3d6338ffe2a8bf5c0e426e5b6d2ebf6
Pedro Lizarraga <plizarraga86@gmail.com>
2022-09-26 19:17:45 -0700

    dynamic transaction list component

--- src/app/components/transanctions-list/transanctions-list.component.ts
+++ src/app/components/transanctions-list/transanctions-list.component.ts
@@ -1,16 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ITransaction } from 'src/app/models/transaction.model';
+import { WalletService } from 'src/app/services/wallet.service';
 
 @Component({
   selector: 'app-transanctions-list',
   templateUrl: './transanctions-list.component.html',
   styleUrls: ['./transanctions-list.component.scss'],
   standalone: true,
+  imports: [CommonModule]
 })
 export class TransanctionsListComponent implements OnInit {
+  
+  transactions$: Observable<ITransaction[]> | null = null;
 
-  constructor() { }
+  constructor(private walletService: WalletService) { }
 
   ngOnInit(): void {
+    this.transactions$ = this.walletService.getAllTransactions();
   }
-
 }
--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -1,56 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { ITransaction } from '../models/transaction.model';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class WalletService {
+	private transactionsMock: ITransaction[] = [
+		{
+			id: 1,
+			account: 'one@example.com',
+			amount: '$100.00',
+			incoming: true,
+			createdAt: '07/09/2022',
+		},
+		{
+			id: 2,
+			account: 'two@example.com',
+			amount: '-$200.00',
+			incoming: false,
+			createdAt: '07/09/2022',
+		},
+		{
+			id: 3,
+			account: 'three@example.com',
+			amount: '$300.00',
+			incoming: true,
+			createdAt: '07/09/2022',
+		},
+		{
+			id: 4,
+			account: 'four@example.com',
+			amount: '-$400.00',
+			incoming: false,
+			createdAt: '07/09/2022',
+		},
+		{
+			id: 5,
+			account: 'five@example.com',
+			amount: '$500.00',
+			incoming: true,
+			createdAt: '07/09/2022',
+		},
+		{
+			id: 6,
+			account: 'six@example.com',
+			amount: '$600.00',
+			incoming: true,
+			createdAt: '07/09/2022',
+		},
+	];
 	constructor() {}
 
-	getAllTransactions(): ITransaction[] {
-		return [
-			{
-				id: 1,
-				account: 'one@example.com',
-				amount: '$100.00',
-				incoming: true,
-				createdAt: '07/09/2022',
-			},
-			{
-				id: 2,
-				account: 'two@example.com',
-				amount: '-$200.00',
-				incoming: false,
-				createdAt: '07/09/2022',
-			},
-			{
-				id: 3,
-				account: 'three@example.com',
-				amount: '$300.00',
-				incoming: true,
-				createdAt: '07/09/2022',
-			},
-			{
-				id: 4,
-				account: 'four@example.com',
-				amount: '-$400.00',
-				incoming: false,
-				createdAt: '07/09/2022',
-			},
-			{
-				id: 5,
-				account: 'five@example.com',
-				amount: '$500.00',
-				incoming: true,
-				createdAt: '07/09/2022',
-			},
-			{
-				id: 6,
-				account: 'six@example.com',
-				amount: '$600.00',
-				incoming: true,
-				createdAt: '07/09/2022',
-			},
-		];
+	getAllTransactions(): Observable<ITransaction[]> {
+		return of(this.transactionsMock);
 	}
 }
