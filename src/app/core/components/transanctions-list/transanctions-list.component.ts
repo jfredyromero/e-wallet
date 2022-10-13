1318600c995525a87cedfdfab7712b67d86298b2
Pedro Lizarraga <plizarraga86@gmail.com>
2022-10-12 23:34:50 -0700

    fixed loading user

--- src/app/core/components/transanctions-list/transanctions-list.component.ts
+++ src/app/core/components/transanctions-list/transanctions-list.component.ts
@@ -3,29 +3,36 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ITransactionResponse } from 'src/app/core/models/transaction-response.model';
 import { WalletService } from 'src/app/core/services/wallet.service';
+import { IUser } from '../../models/user.model';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
-  selector: 'app-transanctions-list',
-  templateUrl: './transanctions-list.component.html',
-  styleUrls: ['./transanctions-list.component.scss'],
-  standalone: true,
-  imports: [CommonModule]
+	selector: 'app-transanctions-list',
+	templateUrl: './transanctions-list.component.html',
+	styleUrls: ['./transanctions-list.component.scss'],
+	standalone: true,
+	imports: [CommonModule],
 })
 export class TransanctionsListComponent implements OnInit {
-  
-  transactions$!: Observable<ITransactionResponse[]>;
+	transactions$!: Observable<ITransactionResponse[]>;
+  user: IUser | null;
 
-  constructor(private walletService: WalletService) { }
-
-  ngOnInit(): void {
-    this.transactions$ = this.walletService.getAllTransactions();
+	constructor(
+		private walletService: WalletService,
+		private authService: AuthService
+	) {
+    this.user = this.authService.currentUserValue;
   }
 
-  isIncoming(transaction: ITransactionResponse): boolean {
-    if (transaction.sender.id === transaction.reciver.id) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+	ngOnInit(): void {
+		this.transactions$ = this.walletService.getAllTransactions();
+	}
+
+	isIncoming(transaction: ITransactionResponse): boolean {
+		if (this.user?.id === transaction.reciver.id) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
