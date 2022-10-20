61a26ca8ae22520816b1e5705de7e94eb3cb3c00
Pedro Lizarraga <plizarraga86@gmail.com>
2022-10-19 19:01:12 -0700

    updated prettier

--- src/app/pages/transactions/transactions-home/transactions-home.component.ts
+++ src/app/pages/transactions/transactions-home/transactions-home.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-transactions-home',
     templateUrl: './transactions-home.component.html',
-    styleUrls: ['./transactions-home.component.scss']
+    styleUrls: ['./transactions-home.component.scss'],
 })
 export class TransactionsHomeComponent {
-
-    constructor() { }
-
+    constructor() {}
 }
